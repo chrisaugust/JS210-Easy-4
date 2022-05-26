c2fbaa2d496c35f2881b78ecfe65542cac8f4c1d
Chris Danielson <christopher.a.danielson@protonmail.com>
2022-05-26 13:41:17 -0600

    Add first solution for 'Find the Duplicate'

---
+++ findDuplicate.js
@@ -0,0 +1,56 @@
+// Find the Duplicate
+//
+// PROBLEM
+// Given an unordered array and the knowledge that exactly one value
+// in the array occurs twice, determine which value occurs twice.
+//
+// EXAMPLES
+// findDup([1, 5, 3, 1]);                                // 1
+// findDup([18,  9, 36, 96, 31, 19, 54, 75, 42, 15,
+//          38, 25, 97, 92, 46, 69, 91, 59, 53, 27,
+//          14, 61, 90, 81,  8, 63, 95, 99, 30, 65,
+//          78, 76, 48, 16, 93, 77, 52, 49, 37, 29,
+//          89, 10, 84,  1, 47, 68, 12, 33, 86, 60,
+//          41, 44, 83, 35, 94, 73, 98,  3, 64, 82,
+//          55, 79, 80, 21, 39, 72, 13, 50,  6, 70,
+//          85, 87, 51, 17, 66, 20, 28, 26,  2, 22,
+//          40, 23, 71, 62, 73, 32, 43, 24,  4, 56,
+//           7, 34, 57, 74, 45, 11, 88, 67,  5, 58]);    // 73
+//
+// ALGORITHM
+// - copy array with slice
+// - sort copiedArray
+// - call forEach on copiedArray
+//   - find index of currentElement and iterate over currentIndex
+//     to array.length - 1
+//        - check all subsequent elements, comparing against value of
+//          currentElement
+//        - return currentElement if there's a match
+//
+// CODE
+'use strict';
+
+function findDup(array) {
+  let duplicate;
+  let copiedArray = array.slice();
+  copiedArray.sort((a, b) => a - b );
+  copiedArray.forEach((elem, index) => {
+    for (let idx = index + 1; idx < array.length; idx += 1) {
+      console.log(`comparing ${elem} with ${copiedArray[idx]}`);
+      if (elem === copiedArray[idx]) duplicate = elem;
+    }
+  });
+  return duplicate;
+}
+// TESTS
+console.log(findDup([1, 5, 3, 1]) === 1);
+console.log(findDup([18,  9, 36, 96, 31, 19, 54, 75, 42, 15,
+  38, 25, 97, 92, 46, 69, 91, 59, 53, 27,
+  14, 61, 90, 81,  8, 63, 95, 99, 30, 65,
+  78, 76, 48, 16, 93, 77, 52, 49, 37, 29,
+  89, 10, 84,  1, 47, 68, 12, 33, 86, 60,
+  41, 44, 83, 35, 94, 73, 98,  3, 64, 82,
+  55, 79, 80, 21, 39, 72, 13, 50,  6, 70,
+  85, 87, 51, 17, 66, 20, 28, 26,  2, 22,
+  40, 23, 71, 62, 73, 32, 43, 24,  4, 56,
+  7, 34, 57, 74, 45, 11, 88, 67,  5, 58]) === 73);
